f5d0d4543f169ee79a0798c99f2aae62bde28a3a
Eliott de Séguier <eliottdes@gmail.com>
2018-06-18 20:14:14 +0200

    [Config] Add Semantic UI and jwt-auth

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SuiModule } from "ng2-semantic-ui";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SuiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
