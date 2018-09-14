ddb6f15afeef2d608c72d76892b1736bee4f0da8
Eliott de Séguier <eliottdes@gmail.com>
2018-09-14 12:21:55 +0200

    [Front] Correct how content is displayed in the viewport

--- front/src/app/common/sidebar/sidebar.component.ts
+++ front/src/app/common/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from "../../auth/auth.service";
 import { Observable } from "rxjs";
+import { AuthService } from "../../auth/auth.service";
 
 @Component({
   selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html'
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
 
