0482fa0707678e31fa1f2f6ae1c6dac40d782ea4
Eliott de Séguier <eliottdes@gmail.com>
2018-09-29 11:08:12 +0200

    [Front] Set tab title on navigation change

--- front/src/app/core/app-routing.module.ts
+++ front/src/app/core/app-routing.module.ts
@@ -1,14 +1,25 @@
-import { NgModule } from "@angular/core";
+import { NgModule, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { Router, RouterModule, Routes } from "@angular/router";
+import { ActivatedRoute, NavigationEnd, Router, RouterModule, Routes } from "@angular/router";
 import { NotFoundComponent } from "./components/not-found/not-found.component";
 import { HomeComponent } from "../modules/showcase/home/home.component";
+import { Title } from "@angular/platform-browser";
+import { filter, map, tap } from "rxjs/operators";
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
+  {
+    path: '',
+    component: HomeComponent
+  },
 
   // otherwise 404
-  { path: '**', component: NotFoundComponent }
+  {
+    path: '**',
+    component: NotFoundComponent,
+    data: {
+      title: 'Page inconnue'
+    }
+  }
 ];
 
 @NgModule({
@@ -21,10 +32,37 @@ const appRoutes: Routes = [
   ]
 })
 export class AppRoutingModule {
-  // Uncomment these following lines to display the router configuration
-  // (useful to see if the routes are in the proper order)
 
-  // constructor(router: Router) {
-  //   console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
-  // }
+  baseTitle = 'Numéris ISEP';
+  separator = ' - ';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private titleService: Title
+  ) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        let child = this.activatedRoute.firstChild;
+        while (child) {
+          if (child.firstChild) {
+            child = child.firstChild;
+          } else if (child.snapshot.data && child.snapshot.data['title']) {
+            return child.snapshot.data['title'];
+          } else {
+            return null;
+          }
+        }
+        return null;
+      })).subscribe( (title: any) => {
+        let finalTitle = this.baseTitle;
+
+        if (title != null) {
+          finalTitle += this.separator + title;
+        }
+
+        this.titleService.setTitle(finalTitle);
+    });
+  }
 }
--- front/src/app/modules/student/student-routing.module.ts
+++ front/src/app/modules/student/student-routing.module.ts
@@ -6,9 +6,30 @@ import { TermsOfUseComponent } from "./terms-of-use/terms-of-use.component";
 import { AuthGuard } from "../../core/guards/auth.guard";
 
 const studentRoutes: Routes = [
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
-  { path: 'terms-of-use', component: TermsOfUseComponent, canActivate: [AuthGuard] }
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+    data: {
+      title: 'Tableau de bord',
+    }
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuard],
+    data: {
+      title: 'Profil',
+    }
+  },
+  {
+    path: 'terms-of-use',
+    component: TermsOfUseComponent,
+    canActivate: [AuthGuard],
+    data: {
+      title: 'Conditions d\'utilisation'
+    }
+  }
 ];
 
 @NgModule({
