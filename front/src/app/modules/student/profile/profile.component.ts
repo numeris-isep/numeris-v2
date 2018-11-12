f30b14fd18f1d3b0282ecc6a6d9d26c0d1b8f897
Eliott de Séguier <eliottdes@gmail.com>
2018-11-12 21:56:47 +1000

    [Front] Add profile page front + add some labels to other components

--- front/src/app/modules/student/profile/profile.component.ts
+++ front/src/app/modules/student/profile/profile.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
-  templateUrl: './profile.component.html'
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
 
--- front/src/app/modules/student/student.module.ts
+++ front/src/app/modules/student/student.module.ts
@@ -5,17 +5,27 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
 import { ProfileComponent } from "./profile/profile.component";
 import { TermsOfUseComponent } from "./terms-of-use/terms-of-use.component";
 import { SuiModule } from "ng2-semantic-ui";
+import { ProfileDetailsComponent } from './profile/profile-details/profile-details.component';
+import { ProfileSummaryComponent } from './profile/profile-summary/profile-summary.component';
+import { SharedModule } from "../../shared/shared.module";
+import { ProfilePreferencesComponent } from './profile/profile-preferences/profile-preferences.component';
+import { ProfileDocumentsComponent } from './profile/profile-documents/profile-documents.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SuiModule,
-    StudentRoutingModule
+    StudentRoutingModule,
+    SharedModule
   ],
   declarations: [
     DashboardComponent,
     ProfileComponent,
-    TermsOfUseComponent
+    TermsOfUseComponent,
+    ProfileDetailsComponent,
+    ProfileSummaryComponent,
+    ProfilePreferencesComponent,
+    ProfileDocumentsComponent
   ]
 })
 export class StudentModule { }
