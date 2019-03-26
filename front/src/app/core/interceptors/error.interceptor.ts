1397e9b607427e180b2fa4663ee5ea619dbddc62
Eliott de Séguier <eliottdes@gmail.com>
2019-03-26 09:53:10 +0100

    [Front] Improve error handler and alert service

--- front/src/app/core/interceptors/error.interceptor.ts
+++ front/src/app/core/interceptors/error.interceptor.ts
@@ -14,15 +14,29 @@ export class ErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
       const error = err.error.error;
+      const status = err.status;
       const url = err.url;
 
-      // Send the alert to the right target
+      let title: string = null;
+      let message: string;
+      let target: string;
+
       switch (true) {
+        case status == 0:
+          title = "Connexion refusée";
+
         case url.includes('/api/login'):
-          this.alertService.error(error, null, false, 'login-form');
+          message = status != 0
+            ? error : "Nous réglons le problème";
+          target = 'login-form';
           break;
 
         default: break;
+      }
+
+      if (message != undefined && target != undefined) {
+        this.alertService.clear();
+        this.alertService.error(message, title, false, target);
       }
 
       return throwError(error);
