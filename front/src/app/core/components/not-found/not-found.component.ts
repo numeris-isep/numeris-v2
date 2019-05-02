4f4c0c5fb83b590d386f890df01a8a18f7fc7e47
Eliott de Séguier <eliottdes@gmail.com>
2019-05-02 21:42:50 +0200

    [Front] Handle 404 error

--- front/src/app/core/components/not-found/not-found.component.ts
+++ front/src/app/core/components/not-found/not-found.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-not-found',
@@ -8,12 +8,12 @@ import { Location } from '@angular/common';
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor(private location: Location) { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
   goBack() {
-    this.location.back()
+    this.router.navigate(['/'])
   }
 }
--- front/src/app/core/interceptors/error.interceptor.ts
+++ front/src/app/core/interceptors/error.interceptor.ts
@@ -3,19 +3,27 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/c
 import { Observable, throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
 import { AlertService } from "../services/alert.service";
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private alertService: AlertService) { }
+  constructor(
+    private alertService: AlertService,
+    private router: Router,
+  ) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
       this.alertService.clear();
 
       switch (err.status) {
+        case 404:
+          this.router.navigate(['/not-found']);
+          break;
+
         case 500:
           this.alertService.error(
             ['Le serveur a rencontré une erreur, veuillez nous contacter si le problème persiste.'],
@@ -35,7 +43,7 @@ export class ErrorInterceptor implements HttpInterceptor {
           break;
       }
 
-      return throwError(err.error.errors)
+      return throwError(err.error.errors[0])
     }));
   }
 }
