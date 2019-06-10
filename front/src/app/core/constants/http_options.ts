50536da13a88efa7439b367dbd886c1ec40c55c2
Eliott de Séguier <eliottdes@gmail.com>
2019-06-10 10:58:01 +0200

    [Docker] Improve docker config for production

--- front/src/app/core/constants/http_options.ts
+++ front/src/app/core/constants/http_options.ts
@@ -1,4 +1,4 @@
-import { HttpHeaders } from "@angular/common/http";
+import { HttpHeaders } from '@angular/common/http';
 
 export const HTTP_OPTIONS = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://api.numeris-isep.fr',
   tokenName: 'numeris_token',
 };
