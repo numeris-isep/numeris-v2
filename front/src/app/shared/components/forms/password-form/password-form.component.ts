70cf0a95fdefe5fc981f000460cd3306efb6db18
Eliott de Séguier <eliottdes@gmail.com>
2019-08-09 17:26:54 +0200

    [Front] Add password strength on password form

--- front/src/app/shared/components/forms/password-form/password-form.component.ts
+++ front/src/app/shared/components/forms/password-form/password-form.component.ts
@@ -9,6 +9,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class PasswordFormComponent implements OnInit {
 
   passwordForm: FormGroup;
+  passwordStrength: number = 0;
+  passwordMatch: boolean = false;
+  strengthMessages: string[] = [];
+
+  requirements = [
+    { regex: /\d/, message: 'contenir au moins 1 chiffre' },
+    { regex: /[a-z]/, message: 'contenir au moins 1 lettre minuscule' },
+    { regex: /[A-Z]/, message: 'contenir au moins 1 lettre majuscule' },
+    { regex: /[$&+,:;=?@#|'<>.^*()%!-]/, message: 'contenir au moins 1 caractère spécial' },
+    { regex: /.{8,}/, message: 'être composé de 8 caractères minimum' },
+  ];
 
   @Input() submitted: boolean;
   @Output() formReady: EventEmitter<FormGroup> = new EventEmitter<FormGroup>();
@@ -27,6 +38,32 @@ export class PasswordFormComponent implements OnInit {
       password: ['', Validators.required],
       password_confirmation: ['', Validators.required],
     });
+  }
+
+  checkStrength() {
+    const password = this.f.password.value.trim();
+    const strengthMessages: string[] = [];
+    this.passwordStrength = 0;
+
+    for (const requirement of this.requirements) {
+      if (requirement.regex.test(password)) {
+        this.passwordStrength++;
+      } else {
+        strengthMessages.push(requirement.message);
+      }
+    }
+
+    this.strengthMessages = strengthMessages;
+    this.checkMatch();
+  }
+
+  checkMatch() {
+    const password = this.f.password.value.trim();
+    const passwordConf = this.f.password_confirmation.value.trim();
+
+    if (password !== '' && passwordConf !== '') {
+      this.passwordMatch = this.f.password.value === this.f.password_confirmation.value;
+    }
   }
 
 }
