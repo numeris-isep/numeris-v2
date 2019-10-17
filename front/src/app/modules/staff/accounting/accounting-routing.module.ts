501c2fa39eba170ceee17978e712276afd6723aa
Eliott de Séguier <eliottdes@gmail.com>
2019-10-17 18:05:59 +0200

    [Front] Fix activate account message error

--- front/src/app/modules/staff/accounting/accounting-routing.module.ts
+++ front/src/app/modules/staff/accounting/accounting-routing.module.ts
@@ -3,31 +3,9 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../../../core/guards/auth.guard';
 import { AccountingIndexComponent } from './accounting-index/accounting-index.component';
-import * as moment from 'moment';
 import { AccountingShowComponent } from './accounting-show/accounting-show.component';
 
-const year = moment().format('Y');
-
 const accountingRoutes: Routes = [
-  // {
-  //   path: 'comptabilite',
-  //   canActivate: [AuthGuard],
-  //   children: [
-  //     {
-  //       path: ':year',
-  //       children: [
-  //         {
-  //           path: ':month',
-  //           component: AccountingShowComponent,
-  //         },
-  //         {
-  //           path: '',
-  //           component: AccountingIndexComponent,
-  //         },
-  //       ],
-  //     },
-  //   ]
-  // },
   {
     path: 'comptabilite/:year/:month',
     canActivate: [AuthGuard],
@@ -43,12 +21,6 @@ const accountingRoutes: Routes = [
     canActivate: [AuthGuard],
     component: AccountingIndexComponent,
   },
-  // {
-  //   path: 'comptabilite',
-  //   redirectTo: 'comptabilite/' + year,
-  //   pathMatch: 'full',
-  //   canActivate: [AuthGuard],
-  // },
 ];
 
 @NgModule({
