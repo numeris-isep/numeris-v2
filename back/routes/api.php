1c06a43ed71464b289a7a73b2dea6437b37de8c2
hugorichier <hugo.richier@hotmail.com>
2022-06-27 19:44:42 +0200

    front/team-images debuged password reset test

--- back/routes/api.php
+++ back/routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('contact-us', 'ContactUsController@contactUs')->name('contact-us');
 
 // Every route in this group require user authentication
-Route::group(['middleware' => 'auth:api'], function () {
+Route::group(['middleware' => 'web'], function () {
 
     // Auth routes
     Route::post('login', 'Auth\AuthController@login')->name('login');
