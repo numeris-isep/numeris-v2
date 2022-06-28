0d5f37cc02bdc1195bc0b80bdaee30fe03a981ae
hugorichier <hugo.richier@hotmail.com>
2022-06-28 08:26:25 +0200

    back/login_tests handing error on test admin logging in

--- back/tests/Feature/Auth/Login/LoginAdministratorTest.php
+++ back/tests/Feature/Auth/Login/LoginAdministratorTest.php
@@ -21,7 +21,7 @@ class LoginAdministratorTest extends TestCase
             'email'     => $user->email,
             'password'  => 'azertyuiop'
         ];
-        $this->withoutExceptionHandling()
+        $this->withoutExceptionHandling();
 
         $this->json('POST', route('login'), $data)
             ->assertStatus(JsonResponse::HTTP_OK)
