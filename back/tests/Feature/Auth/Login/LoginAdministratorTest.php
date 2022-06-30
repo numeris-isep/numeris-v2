786d7a2ca36b9da9b97d755efd84c4bc614786e9
hugorichier <hugo.richier@hotmail.com>
2022-06-30 03:46:44 +0200

    back/login_tests test build

--- back/tests/Feature/Auth/Login/LoginAdministratorTest.php
+++ back/tests/Feature/Auth/Login/LoginAdministratorTest.php
@@ -22,7 +22,6 @@ class LoginAdministratorTest extends TestCase
             'password'  => 'azertyuiop'
         ];
 
-
         $this->json('POST', route('login'), $data)
             ->assertStatus(JsonResponse::HTTP_OK)
             ->assertJsonStructure(['access_token']);
