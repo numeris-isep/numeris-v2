13f023260b80a0eb0ed8f38b751899bd0a423ef1
Marc Partensky <marc.partensky@gmail.com>
2022-10-17 06:17:00 +0200

    Revert "not clearing config"
    This reverts commit eac0beaba4e125cdc311ccc52af25e2572ef6096.

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -3,7 +3,7 @@
 php artisan key:generate --force
 php artisan jwt:secret --force
 php artisan cache:clear
-# php artisan config:clear
+php artisan config:clear
 php artisan migrate:refresh --seed --force
 
 docker-php-entrypoint
