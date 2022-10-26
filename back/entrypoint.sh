9009cc8a5e85e22a62abcd1f75b904ba27cd3b1a
Marc Partensky <marc.partensky@gmail.com>
2022-10-26 06:17:08 +0200

    fuck seed

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -4,7 +4,7 @@ php artisan key:generate --force
 php artisan jwt:secret --force
 php artisan cache:clear
 php artisan config:clear
-php artisan migrate:refresh --seed --force
+php artisan migrate:refresh --force
 
 docker-php-entrypoint
 apache2-foreground
