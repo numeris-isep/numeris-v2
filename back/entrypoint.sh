2872d48f1c0a1865ed8d55aa814c2ce6c050c9c2
Marc Partensky <marc.partensky@gmail.com>
2022-10-26 06:32:06 +0200

    trying without force

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
-php artisan key:generate --force
-php artisan jwt:secret --force
+php artisan key:generate
+# php artisan key:generate --force
+php artisan jwt:secret
+# php artisan jwt:secret --force
 # php artisan cache:clear
 php artisan config:clear
-php artisan migrate:refresh --force
+php artisan migrate:refresh
 
 docker-php-entrypoint
 apache2-foreground
